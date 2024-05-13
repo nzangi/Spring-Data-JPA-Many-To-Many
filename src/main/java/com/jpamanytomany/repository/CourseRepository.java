e9e37ddbdb8d0082caf8897ad51d56380ae0c97f
nzangi <nzangimuoki284@gmail.com>
2024-05-13 20:36:27 +0300

    Worked on Spring Data JPA many to many

--- src/main/java/com/jpamanytomany/repository/CourseRepository.java
+++ src/main/java/com/jpamanytomany/repository/CourseRepository.java
@@ -2,9 +2,11 @@ package com.jpamanytomany.repository;
 
 import com.jpamanytomany.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course,Integer> {
     List<Course> findByCourseFeeLessThan(double courseFee);
 }
--- src/main/java/com/jpamanytomany/repository/StudentRepository.java
+++ src/main/java/com/jpamanytomany/repository/StudentRepository.java
@@ -2,9 +2,11 @@ package com.jpamanytomany.repository;
 
 import com.jpamanytomany.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student,Integer> {
     List<Student> findByStudentNameContaining(String studentName);
 }
